5cb3f032e847eae5ab2d2e24c8fde1597d723c69
NeoBots Programmers <neobots2903@gmail.com>
2022-03-04 19:38:49 -0800

    jess

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,10 +9,10 @@ public class RobotMap {
    public static final int backRightMotor = 4; //3
    public static final int lowerShoot =  5; //6
    public static final int upperShoot = 6; //7
-   public static final int climb = 7; //10
+   public static final int climb = 7; //11
    public static final int upperIntake = 8; //8
-   public static final int indexer = 9; //9
-   public static final int otherIntake = 12; 
+   public static final int indexer = 9; //10
+   public static final int otherIntake = 12; //9
    
    //TalonSRX motors 
    public static final int pivot = 10; //4
